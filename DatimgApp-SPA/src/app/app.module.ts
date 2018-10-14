c1e1f88724ee1743afff7d9791ce90a7c23b6120
legobrokkori <ralirulego103@gmail.com>
2018-10-14 16:29:33 +0900

    add error hundling

--- DatimgApp-SPA/src/app/app.module.ts
+++ DatimgApp-SPA/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService } from 'src/app/_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { RegisterComponent } from './register/register.component';
     FormsModule
   ],
   providers: [
-    AuthService
+    AuthService,
+    ErrorInterceptorProvider
   ],
   bootstrap: [AppComponent]
 })
