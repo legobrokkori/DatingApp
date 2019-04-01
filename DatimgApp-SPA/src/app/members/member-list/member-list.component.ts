4b8b0764d57bd81b91e52d364d8db5ac2eef6354
legobrokkori <ralirulego103@gmail.com>
2019-04-01 10:57:33 +0900

    building a great looking user interface

---
+++ DatimgApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { User } from 'src/app/_models/user';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+
+  constructor(private userServise: UserService, private alertify: AlertifyService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.users = data['users'];
+    });
+  }
+
+  // loadUsers() {
+  //   this.userServise.getUsers().subscribe((users: User[]) => {
+  //     this.users = users;
+  //   }, error => {
+  //     this.alertify.error(error);
+  //   });
+  // }
+}
--- DatimgApp-SPA/src/app/routes.ts
+++ DatimgApp-SPA/src/app/routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from 'src/app/home/home.component';
-import { MemberListComponent } from 'src/app/member-list/member-list.component';
+import { MemberListComponent } from 'src/app/members/member-list/member-list.component';
 import { MessagesComponent } from 'src/app/messages/messages.component';
 import { ListsComponent } from 'src/app/lists/lists.component';
 import { AuthGuard } from 'src/app/_guards/auth.guard';
+import { MemberDetailComponent } from 'src/app/members/member-detail/member-detail.component';
+import { MemberDetailResolver } from 'src/app/_resolvers/member-detail.resolver';
+import { MemberListResolver } from 'src/app/_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
@@ -12,7 +15,9 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+            { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+            { path: 'members/:id', component: MemberDetailComponent,
+                resolve: {user: MemberDetailResolver}},
             { path: 'messages', component: MessagesComponent},
             { path: 'lists', component: ListsComponent},
         ]
